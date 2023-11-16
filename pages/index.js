9f53cffcfa46feebef5b01dece1669d4c4714677
Reza Lyon Kurniawan <rezalyonk01@gmail.com>
2023-11-16 23:52:21 +0700

    changes

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,19 @@ import Head from 'next/head'
 import Image from 'next/image'
 // import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import Navbars from '@/components/Navbars/indexPageNavbars'
+import Hero from '@/components/Hero/indexPageHero'
 
 // const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
-      <h1>Index Home Loh</h1>
+      <div className={`${styles.containerDisplay} container-fluid`}>
+        {/* <h1>Index Home Loh</h1> */}
+        <Navbars />
+        <Hero />
+      </div>
     </>
   )
 }
