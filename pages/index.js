d777ee0f5c7e27b8991bed73decf5def633ba690
Reza Lyon Kurniawan <rezalyonk01@gmail.com>
2023-11-17 11:23:29 +0700

    chnages index

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
 import Navbars from '@/components/Navbars/indexPageNavbars'
 import Hero from '@/components/Hero/indexPageHero'
+import Footers from '@/components/Footers/indexPageFooters'
 
 // const inter = Inter({ subsets: ['latin'] })
 
@@ -14,6 +15,7 @@ export default function Home() {
         {/* <h1>Index Home Loh</h1> */}
         <Navbars />
         <Hero />
+        <Footers />
       </div>
     </>
   )
